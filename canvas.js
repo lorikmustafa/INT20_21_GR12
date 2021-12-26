094e8c54de062b422e6c161d1f1aeeed18515d62
lorikmustafa <lorik.mustafa2@student.uni-pr.edu>
2021-12-26 19:11:36 +0100

    Added canvas

---
+++ canvas.js
@@ -0,0 +1,31 @@
+var canvas = document.querySelector('canvas');
+var c = canvas.getContext("2d");
+c.beginPath();
+c.arc(60, 70, 43, 0, 2 * Math.PI);
+c.lineWidth=8;
+c.strokeStyle="#000000";
+c.stroke();
+
+c.beginPath();
+c.arc(115, 70, 43, 0, 2 * Math.PI);
+c.lineWidth=8;
+c.strokeStyle="#000000";
+c.stroke();
+
+c.beginPath();
+c.arc(170, 70, 43, 0, 2 * Math.PI);
+c.lineWidth=8;
+c.strokeStyle="#000000";
+c.stroke();
+
+c.beginPath();
+c.arc(225, 70, 43, 0, 2 * Math.PI);
+c.lineWidth=8;
+c.strokeStyle="#000000";
+c.stroke();
+
+
+//c.fillRect(100,100,100,100);
+console.log(canvas);
+
+
