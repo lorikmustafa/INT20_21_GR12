93de0d0b6e9fa47c1972a3d5e96962a9ad0d4eb3
meritonkryeziu0 <meritonkryeziu0@gmail.com>
2021-12-28 14:12:00 +0100

    added drag and drop function

---
+++ counter.js
@@ -0,0 +1,17 @@
+function animateValue(id, start, end, duration) {
+    if (start === end) return;
+    var range = end - start;
+    var current = start;
+    var increment = end > start? 1 : -1;
+    var stepTime = Math.abs(Math.floor(duration / range));
+    var obj = document.getElementById(id);
+    var timer = setInterval(function() {
+        current += increment;
+        obj.innerHTML = current;
+        if (current == end) {
+            clearInterval(timer);
+        }
+    }, stepTime);
+}
+
+animateValue("value", 0, 5602, 5000);
