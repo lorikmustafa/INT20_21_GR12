238ad4555452dce31d9d4a88ec1ac41a6b46aedd
lorentsinani <lorent.sinani@student.uni-pr.edu>
2021-12-29 16:38:38 +0100

    aniamtion

--- products.js
+++ products.js
@@ -1,13 +1,12 @@
 window.addEventListener('click', (a) => {
 
-    const rond = document.createElement('div');
-    rond.className = 'AnimationClick';
-    rond.style.top = `${a.pageY - 50}px`;
-    rond.style.left = `${a.pageX - 50}px`;
-    document.body.appendChild(rond);
+    const pika = document.createElement('div');
+    pika.className = 'AnimationClick';
+    pika.style.top = `${a.pageY - 50}px`;
+    pika.style.left = `${a.pageX - 50}px`;
+    document.body.appendChild(pika);
 
     setTimeout(() => {
-       rond.remove();
-   }, 1500)
+        pika.remove();
+    }, 1500)
 })
- 
